a4828454658dcc103c95f37f24b1f4873c24a85a
Dylan Gough <dylangough35@gmail.com>
2020-08-17 20:47:48 -0400

    changes

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 const makeReadme = require("./generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
 inquirer.prompt([
